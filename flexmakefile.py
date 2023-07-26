99f51cb36110a1acdfd712ecc857cd6b1063cf9f
Hinase <112501342+HinaseDev@users.noreply.github.com>
2023-07-26 20:52:53 +0000

    Create flexmakefile.py

---
+++ flexmakefile.py
@@ -0,0 +1,10 @@
+# Usually you would install the Package here
+
+def prepare():
+    print("This is where you would do preparations like downloading external files")
+
+def build():
+    print("Here you would build, compile and install the Package")
+
+def finish():
+    print("Final Cleanup Steps")
